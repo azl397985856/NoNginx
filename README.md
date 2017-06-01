cd0659e935d59ecf12a80ff999392d04d558b2b3
Lu zhipeng <zplu@800best.com>
2017-06-01 15:20:53 +0800

    v2.0 initial commit

--- README.md
+++ README.md
@@ -3,10 +3,9 @@
 # NoNginx
 [![Build Status](https://travis-ci.org/azl397985856/NoNginx.png)](https://travis-ci.org/azl397985856/NoNginx)
 ![Coverage Status](https://coveralls.io/repos/azl397985856/NoNginx/badge.svg?branch=master)
-##frontEnd to backEnd integrated test web page.
+## Intro
 FrontEnd devers can fetch data from mockServer when they devs locally . But it's time to get out of the greehouse when they have to make a integrated test with the backend. We didnt wanna rely on others as usual, but now we have to face.
-So I built that web page for you.  What you should do is just inputing the address of the backend, and click the button.(allright, i confess, you have to connect to my express server first!)
-![image](https://github.com/azl397985856/NoNginx/raw/master/clip.png)
+that 's a proxy to dispatch all the requests including static assets and backend endpoints at dev-env
 
 ## License
 
--- app.js
+++ app.js
@@ -1,12 +1,8 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
 var session = require('express-session');
-
-var routes = require('./routes/index');
 
 var app = express();
 app.use(session({
@@ -16,20 +12,21 @@ app.use(session({
   resave: false,
   saveUninitialized: true,
   secret: 'api-mock-server'
-}));-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+}));
 
-// uncomment after placing your favicon in /public
-// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
+app.use('/', function(req, res, next) {
+  var path = req.path;
+  var matched = path.match(/\.[html|js|css|jpg|jpeg|png]$/);
+  if (matched) {
+      // 静态请求，转发到webpack-dev-server
+  } else {
+      // 动态请求，转发到后台endpoints
+  }
+
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
